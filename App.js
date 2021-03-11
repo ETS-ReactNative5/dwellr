dd099eed878e5cde8e3930ad75030ffa2c3e39a8
Sally Thompson <sft259@nyu.edu>
2021-03-11 00:13:59 -0500

    an almost functioning button
    you can click it, but I can't get the style stuff to appear idk wtf I'm doing hahaha

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ProgressViewIOSComponent, StyleSheet, Text, View } from 'react-native';
+import CustomButton from './Components/CustomButton';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>dwellr</Text>
+
+      <CustomButton> 
+        <Text> Login </Text>
+      </CustomButton>
+
+
       <StatusBar style="auto" />
     </View>
   );
---
+++ Components/CustomButton.js
@@ -0,0 +1,34 @@
+import React from 'react'; 
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
+
+const CustomButton = props => {
+    return(
+        <TouchableOpacity onPress={props.onPress}>
+            <View style={{...styles.button, ...props.style}}>
+
+                <Text style={{...styles.buttonText, ...props.textStyling}}>
+                    {props.children}
+                </Text>
+
+
+            </View>
+        </TouchableOpacity>
+    );
+};
+
+const styles= StyleSheet.create({
+    button: {
+        backgroundColor: 'black', 
+        paddingVertical: 20, 
+        paddingHorizontal: 25,
+        borderRadius: 25
+    },
+
+    buttonText: {
+        color: 'white', 
+        fontSize: 18, 
+
+    },
+});
+
+export default CustomButton; 
